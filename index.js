afcfeb99070e1c63da63b3a2cd99cd4bca1afabd
andrewhahn1 <153559295+andrewhahn1@users.noreply.github.com>
2024-02-14 14:59:55 -0500

    Update index.js
    If committed, will fulfill the requirements of 2.4

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
-const express = require("express");
+const express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path');
+
 const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
+
+app.use(morgan('combined', {stream: accessLogStream}));
+
+let topMovies = [
+    {
+        title: 'The Farewell'
+    },
+    {
+        title: 'Mulan'
+    },
+    {
+        title: 'The Lord of the Rings: The Fellowship of the Ring'
+    },
+    {
+        title: 'The Lord of the Rings: The Two Towers'
+    },
+    {
+        title: 'The Lord of the Rings: Return of the King'
+    },
+    {
+        title: 'Dan in Real Life'
+    },
+    {
+        title: 'Weekend'
+    },
+    {
+        title: 'All of Us Strangers'
+    },
+    {
+        title: 'Looking'
+    },
+    {
+        title: 'Princess Mononoke'
+    },
+];
+
+// GET requests
+app.get('/', (req, res) => {
+    res.send('Welcome to MyFlix!');
+});
+
+app.use(express.static('public'));
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
