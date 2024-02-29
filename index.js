a874f7e2e67ff7e0ecde360292ae729a6db6ca1c
andrewhahn1 <153559295+andrewhahn1@users.noreply.github.com>
2024-02-29 12:18:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,252 +1,131 @@
-const bodyParser = require('body-parser');
 const express = require('express'),
-    morgan = require('morgan'),
-    fs = require('fs'),
-    path = require('path'),
-    uuid = require('uuid');
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
 
+const mongoose = require('mongoose');
+
+const morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path');
+    
 const app = express();
+
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Director;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'});
 
 app.use(morgan('combined', {stream: accessLogStream}));
 app.use(bodyParser.json());
 
-let users = [
-    {
-        id: 1,
-        name: "Evan",
-        favoriteMovies: []
-    },
-    {   
-        id: 2,
-        name: "Alexis",
-        favoriteMovies: []
-    },
-]
 
-let movies = [
-    {
-        "Title": "The Farewell",
-        "Description": "A Chinese family discovers their grandmother has only a short while left to live and decide to keep her in the dark, scheduling a wedding to gather before she dies.",
-        "Genre": {
-            "Name": "Drama",
-            "Description":"A genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-        },
-        "Director": {
-            "Name": "Lulu Wang",
-            "Bio": "Lulu Wang was born February 25, 1983 and is a Chinese-born American filmmaker. She is best known for writing and directing the comedy-drama films Posthumous (2014) and The Farewell (2019). For the latter, she received the Independent Spirit Award for Best Film and the film was named one of the top ten films of 2019 by the American Film Institute. Wang has also written, produced, and directed several short films, documentaries, and music videos.",
-            "Birth": 1983.0
-        },
-        "ImageURL":"https://images.app.goo.gl/Tn4jRnSP5xyXZqTP9",
-        "Featured": false
-    },
-    {
-        "Title": "Mulan",
-        "Description": "To save her father from death in the army, a young maiden secretly goes in his place and becomes one of China's greatest heroines in the process.",
-        "Genre": {
-            "Name": "Action",
-            "Description":"A genre featuring characters involved in exciting and usually dangerous activities and adventures."
-        },
-        "Director": {
-            "Name": "Tony Bancroft",
-            "Bio": "Tony Bancroft is an American animator and film director who frequently collaborates with Disney. He is the founder and owner of the faith-driven animation company Toonacious Family Entertainment. Tony is the Executive VP Creative Development and Production for DivideNine Animation Studios.",
-            "Birth": 1967.0
-        },
-        "ImageURL":"https://images.app.goo.gl/s9DcrURzwNYdtdNs8",
-        "Featured": false
-    },
-    {
-        "Title": "The Ring",
-        "Description": "A journalist must investigate a mysterious videotape which seems to cause the death of anyone one week to the day after they view it.",
-        "Genre": {
-            "Name": "Horror",
-            "Description": "A film in which very frightening or unnatural things happen."
-        },
-        "Director": {
-            "Name": "Gore Verbinski",
-            "Bio": "Gregor Justin 'Gore' Verbinski (born March 16, 1964) is an American film director. He is best known for directing Mouse Hunt, The Ring, the first three Pirates of the Caribbean films, and Rango. For his work on Rango, Verbinski won both the Academy Award and BAFTA Award for Best Animated Film.",
-            "Birth": 1964.0
-        },
-        "ImageURL":"https://images.app.goo.gl/LZHn2QXMAcF7qcmz5",
-        "Featured": false
-    },
-    {
-        "Title": "Mean Girls",
-        "Description": "Cady Heron is a hit with The Plastics, the A-list girl clique at her new school, until she makes the mistake of falling for Aaron Samuels, the ex-boyfriend of alpha Plastic Regina George.",
-        "Genre": {
-            "Name": "Comedy",
-            "Description": "A comedy film is a category of film that emphasizes humor. These films are designed to amuse audiences and make them laugh. Films in this genre typically have a happy ending, with dark comedy being an exception to this rule. Comedy is one of the oldest genres in film, and it is derived from classical comedy in theatre."
-        },
-        "Director": {
-            "Name": "Mark Waters",
-            "Bio": "Mark Waters grew up in South Bend, Indiana, USA. His first film, The House of Yes, debuted at the Sundance Film Festival in 1997. He is a director and producer, known for Mean Girls (2004), 500 Days of Summer (2009) and He's All That (2021).",
-            "Birth": 1964.0
-        },
-        "ImageURL":"https://images.app.goo.gl/8gQ3QDBNjBynvRk97",
-        "Featured": false
-    },
-    {
-        "Title": "Sordid Lives",
-        "Description": "As three generations of a family in a small Texas town gather for a funeral, we learn the hilarious, sad, trashy truth of their 'Sordid Lives.'",
-        "Genre": {
-            "Name": "Comedy",
-            "Description": "A comedy film is a category of film that emphasizes humor. These films are designed to amuse audiences and make them laugh.[1] Films in this genre typically have a happy ending, with dark comedy being an exception to this rule. Comedy is one of the oldest genres in film, and it is derived from classical comedy in theatre."
-        },
-        "Director": {
-            "Name": "Del Shores",
-            "Bio": "Delferd Lynn Shores is an American film director, television writer, producer, playwright, standup comedian, and actor. He has written, directed and produced across studio and independent film, network and cable television and Los Angeles, regional and national touring theatre.",
-            "Birth": 1957.0
-        },
-        "ImageURL":"https://images.app.goo.gl/GsBxCF1yRjpXVEH9A",
-        "Featured": false
-    },
-    {
-        "Title": "Dan in Real Life",
-        "Description": "A widower finds out the woman he fell in love with is his brother's girlfriend.",
-        "Genre": {
-            "Name": "Drama",
-            "Description":"A genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-        },
-        "Director": {
-            "Name": "Peter Hedges",
-            "Bio": "Peter Simpson Hedges (born July 6, 1962) is an American novelist, playwright, screenwriter, film director and film producer.",
-            "Birth": 1962.0
-        },
-        "ImageURL":"https://images.app.goo.gl/ACVmQ81cJ8wCu9Zs5",
-        "Featured": false
-    },
-    {
-        "Title": "Weekend",
-        "Description": "After a drunken house party with his straight mates, Russell heads out to a gay club. Just before closing time he picks up Glen but what's expected to be just a one-night stand becomes something else, something special.",
-        "Genre": {
-            "Name": "Drama",
-            "Description":"A genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-        },
-        "Director": {
-            "Name": "Andrew Haigh",
-            "Bio": "Andrew Haigh is an English filmmaker. He is best known for writing and directing the films Weekend, 45 Years, Lean on Pete, and All of Us Strangers. He also wrote and produced the HBO series Looking and its film sequel Looking: The Movie, as well as the BBC Two limited series The North Water.",
-            "Birth": 1973.0
-        },
-        "ImageURL":"https://images.app.goo.gl/rJ2To1tiVnhQMCzm7",
-        "Featured": false
-    },
-    {
-        "Title": "All of Us Strangers",
-        "Description": "A screenwriter drawn back to his childhood home enters into a fledgling relationship with a mysterious neighbor as he then discovers his parents appear to be living just as they were on the day they died, 30 years before.",
-        "Genre": {
-            "Name": "Drama",
-            "Description":"A genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-        },
-        "Director": {
-            "Name": "Andrew Haigh",
-            "Bio": "Andrew Haigh is an English filmmaker. He is best known for writing and directing the films Weekend, 45 Years, Lean on Pete, and All of Us Strangers. He also wrote and produced the HBO series Looking and its film sequel Looking: The Movie, as well as the BBC Two limited series The North Water.",
-            "Birth": 1973.0
-        },
-        "ImageURL":"https://images.app.goo.gl/NNYtvwb9PbLbUrG86",
-        "Featured": false
-    },
-    {
-        "Title": "Looking",
-        "Description": "Patrick returns to San Francisco in search of closure and resolution regarding his relationships with Richie and Kevin.",
-        "Genre": {
-            "Name": "Drama",
-            "Description":"A genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
-        },
-        "Director": {
-            "Name": "Andrew Haigh",
-            "Bio": "Andrew Haigh is an English filmmaker. He is best known for writing and directing the films Weekend, 45 Years, Lean on Pete, and All of Us Strangers. He also wrote and produced the HBO series Looking and its film sequel Looking: The Movie, as well as the BBC Two limited series The North Water.",
-            "Birth": 1973.0
-        },
-        "ImageURL":"https://images.app.goo.gl/5cEGDDKqxCJ7wEHr9",
-        "Featured": false
-    },
-    {
-        "Title": "Princess Mononoke",
-        "Description": "On a journey to find the cure for a Tatarigami's curse, Ashitaka finds himself in the middle of a war between the forest gods and Tatara, a mining colony. In this quest he also meets San, the Mononoke Hime.",
-        "Genre": {
-            "Name": "Fantasy",
-            "Description": "A genre with fantastic themes, usually magic, supernatural events, mythology, folklore, or exotic fantasy worlds. The genre is considered a form of speculative fiction alongside science fiction films and horror films, although the genres do overlap. Fantasy films often have an element of magic, myth, wonder, escapism, and the extraordinary."
-        },
-        "Director": {
-            "Name": "Hayao Miyazaki",
-            "Bio": "Hayao Miyazaki is a Japanese animator, filmmaker, and manga artist. A co-founder of Studio Ghibli, he has attained international acclaim as a masterful storyteller and creator of Japanese animated feature films, and is widely regarded as one of the most accomplished filmmakers in the history of animation.",
-            "Birth": 1941.0
-        },
-        "ImageURL": "https://images.app.goo.gl/dNdQw2ecmp7MmXpUA",
-        "Featured": false
-    },
-];
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 // CREATE - New users
-app.post('/users', (req, res) => {
-    const newUser = req.body;
-
-    if(newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send ('users need names')
-    }
-})
+app.post('/users', async (req, res) => {
+    await Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Name: req.body.Name,
+              Username: req.body.Username,
+              Password: req.body.Password,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
 
 // UPDATE  - User info
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedUser = req.body;
-    
-    let user = users.find(user => user.id == id);
-
-    if(user) {
-        user.name = updatedUser.name;
-        res.status(200).json(user);
-    } else {
-        res.status(400).send('no such user')
-    }
-})
+app.put('/users/:Username', async (req, res) => {
+    await Users.findOneAndUpdate({ Username: req.params.Username }, 
+        { 
+        $set:{
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true }) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    })
+  
+  });
 
 // CREATE  - Add movie to favorites
-app.post('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
-    
-    let user = users.find(user => user.id == id);
-
-    if(user) {
-        user.favoriteMovies.push(movieTitle);
-        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-    } else {
-        res.status(400).send('no such user')
-    }
-})
+app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+    await Users.findOneAndUpdate({ Username: req.params.Username }, {
+       $push: { FavoriteMovies: req.params.MovieID }
+     },
+     { new: true })
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+  });
 
 
 // DELETE  - Remove movie from favorites
-app.delete('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params;
-    
-    let user = users.find(user => user.id == id);
-
-    if(user) {
-        user.favoriteMovies = user.favoriteMovies.filter(title => title !== movieTitle);
-        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-    } else {
-        res.status(400).send('no such user')
-    }
-})
+app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
+    await Users.findOneAndUpdate(
+        { Username: req.params.Username },
+        { $pull: { FavoriteMovies: req.params.MovieID } },
+        { new: true }
+    )
+    .then((updatedUser) => {
+        res.json(updatedUser);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+    });
+});
 
 // DELETE  - Existing users to deregister
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params;
-    
-    let user = users.find(user => user.id != id);
-
-    if(user) {
-        users = users.filter(user => user.id );
-        res.status(200).send(`user ${id} has been deleted`);
-    } else {
-        res.status(400).send('no such user')
-    }
-})
+app.delete("/users/:Username", (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+        if (!user) {
+            res.status(400).send(req.params.Username + " was not found");
+        } else {
+            res.status(200).send(req.params.Username + " was deleted.");
+        }
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+    });
+});
 
 
 // GET requests
@@ -257,45 +136,52 @@ app.get('/', (req, res) => {
 app.use('public/documentation.html', express.static('public'));
 
 // READ - Return list of all movies
-app.get('/movies', (req, res) => {
-    res.status(200).json(movies);
-})
+app.get('/movies', async (req, res) => {
+    await Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
 
 // READ - Return data about a single movie by title
-app.get('/movies/:title', (req, res) => {
-    const { title } = req.params;
-    const movie = movies.find(movie => movie.Title === title);
-
-    if (movie) {
-        res.status(200).json(movie);
-    } else {
-        res.status(400).send('no such movie')
-    }
-})
+app.get('/movies/:Title', (req, res) => {
+    Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+    res.json(movie);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+    });
+});
 
 // READ - Return data about genre
-app.get('/movies/genre/:genreName', (req, res) => {
-    const { genreName } = req.params;
-    const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
-
-    if (genre) {
-        res.status(200).json(genre);
-    } else {
-        res.status(400).send('no such genre')
-    }
-})
+app.get('/genre/:Name', (req, res) => {
+    Genres.findOne({ Name: req.params.Title })
+    .then((genre) => {
+    res.json(genre.Description);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+    });
+});
 
 // READ - Return data about a director
-app.get('/movies/directors/:directorName', (req, res) => {
-    const { directorName } = req.params;
-    const director = movies.find(movie => movie.Director.Name === directorName).Director;
-
-    if (director) {
-        res.status(200).json(director);
-    } else {
-        res.status(400).send('no such director')
-    }
-})
+app.get('/director/:Name', (req, res) => {
+    Movies.findOne({ Name: req.params.Title })
+    .then((director) => {
+    res.json(director);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+    });
+});
 
 
 
